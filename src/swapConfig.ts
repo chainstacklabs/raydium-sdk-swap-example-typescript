1f8362616c0522021e79fb66d0ca644b0d3eab6a
Soos3D <99700157+soos3d@users.noreply.github.com>
2024-03-19 10:47:42 -0300

    Fix wrong address in swap config

--- src/swapConfig.ts
+++ src/swapConfig.ts
@@ -3,7 +3,7 @@ export const swapConfig = {
   useVersionedTransaction: true,
   tokenAAmount: 0.01, // Swap 0.01 SOL for USDT in this example
   tokenAAddress: "So11111111111111111111111111111111111111112", // Token to swap for the other, SOL in this case
-  tokenBAddress: "FN2dH9S96vtqTBSHa5snvcRG3BbWjwQC8P1XcgZNtWfQ", // USDC address
+  tokenBAddress: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", // USDC address
   maxLamports: 1000000, // Max lamports allowed for fees
   direction: "in" as "in" | "out", // Swap direction: 'in' or 'out'
   liquidityFile: "https://api.raydium.io/v2/sdk/liquidity/mainnet.json",
