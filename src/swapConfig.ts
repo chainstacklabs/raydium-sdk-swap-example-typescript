dfc55a9d1852d51bae5c40ed75f0d595552e7f72
Soos3D <99700157+soos3d@users.noreply.github.com>
2024-03-20 11:54:09 -0300

    Update swapConfig.ts

--- src/swapConfig.ts
+++ src/swapConfig.ts
@@ -1,10 +1,10 @@
 export const swapConfig = {
-  executeSwap: true, // Send tx when true, simulate tx when false
+  executeSwap: false, // Send tx when true, simulate tx when false
   useVersionedTransaction: true,
   tokenAAmount: 0.01, // Swap 0.01 SOL for USDT in this example
   tokenAAddress: "So11111111111111111111111111111111111111112", // Token to swap for the other, SOL in this case
   tokenBAddress: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v", // USDC address
-  maxLamports: 1000000, // Max lamports allowed for fees
+  maxLamports: 1500000, // Micro lamports for priority fee
   direction: "in" as "in" | "out", // Swap direction: 'in' or 'out'
   liquidityFile: "https://api.raydium.io/v2/sdk/liquidity/mainnet.json",
   maxRetries: 20,
